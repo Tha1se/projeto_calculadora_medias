83bdc3ef230cef29473cc51b71cba8484e4166ca
Thaise <thaise.zago@hotmail.com>
2025-02-10 18:31:44 -0300

    first commit

--- main.js
+++ main.js
@@ -1,63 +1,39 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando"/>'; //inserindo emojis de aprovação ou reprovação;
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado"/>';
-const atividades = []; //isto é um array, criado para somar todas as atividades
-const notas = []; //isto é um array, criado para somar todas as notas
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt("Digite a nota mínima: "));//permite ao usuário personalizar a nota mínima;
+const form = document.getElementById('form-agenda');
 
-let linhas ='';//variável criada para que várias matérias e notas sejam inseridas e a tabela seja alimentada criando novas linhas a cada conteúdo, e não substituindo o valor a cada nova inserção. Fica no topo do código (valor global) pois se ficar dentro da tag do Listener ela será resetada a cada nova inserção.
+let linhas ='';
+const nomes = [];// Criando o array para armazenar os nomes
+const telefones = [];// Criando o array para armazenar os telefones
 
 form.addEventListener('submit', function (e){
-    e.preventDefault();//remover função automática de atualizar a página quando apertar "Adicionar+"
+    e.preventDefault();
 
-    adicionaLinha(); //autoexplicativo (chama a função de add a linha ao clicar no botão "Adicionar+")
-    atualizaTabela(); //chama a função de inserir dados na tabela, logo após ter criado nova linha para tal
-    atualizaMediaFinal();
+    adicionaLinha();
+    atualizaTabela();
 });
 
-function adicionaLinha() { //este trecho inicialmente foi criado dentro do 'addEventListener' mas foi separado posteriormente para ele não ficar muito cheio
-    const inputNomeAtividade = document.getElementById('nome-atividade'); //capturando campos de preenchimento
-    const inputNotaAtividade = document.getElementById('nota-atividade'); //capturando campos de preenchimento
+function adicionaLinha() { 
+    const inputNome = document.getElementById('nome'); //capturando dados dos campos de preenchimento
+    const inputTelefone = document.getElementById('telefone'); //capturando dados dos campos de preenchimento
 
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade "${inputNomeAtividade.value}" já foi inserida.`)
+    if (nomes.includes(inputNome.value)) {
+        alert(`O contato de "${inputNome.value}" já foi inserido.`)
     } else {
-        atividades.push(inputNomeAtividade.value); // o PUSH irá transmitir os valores para a constante (atividades) criada para somar todas as atividades
-        notas.push(parseFloat(inputNotaAtividade.value)); // o PUSH irá transmitir os valores para a constante (notas) criada para somar todas as notas. O parseFloat converte os valores informados em 'string' para números, permitindo a soma das médias
+        nomes.push(inputNome.value); //Adicionando o nome à lista
+        telefones.push(inputTelefone.value); //Adicionando o telefone à lista
 
         let linha = '<tr>';//abertura da tag 'tr' para criação da variável que receberá o código html como uma string
-            linha += `<td>${inputNomeAtividade.value}</td>`; //Esta atribuição é o que faz que a linha seja preenchida com os dados informados pelo usuário.
-            linha += `<td>${inputNotaAtividade.value}</td>`;
-            linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`; //Efeito ternário. Neste caso '?' serve como 'if' e ':' serve como 'else'
-            linha += '</tr>';//fechamento da tag 'tr' 
+        linha += `<td>${inputNome.value}</td>`; //Esta atribuição é o que faz que a linha seja preenchida com os dados informados pelo usuário.
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += '</tr>';//fechamento da tag 'tr' 
 
         linhas += linha;//'+=' é uma concatenação, também poderia ter sido usado: 'linhas = linha + 'outro conteúdo'. 
     }
 
-    inputNomeAtividade.value = '';//limpa os campos de preenchimento após os dados serem inseridos, liberando assim espaço para uma nova inserção sem o usuário precisar limpar manualmente os campos
-    inputNotaAtividade.value = '';
+    inputNome.value = '';//limpa os campos de preenchimento após os dados serem inseridos, liberando assim espaço para uma nova inserção sem o usuário precisar limpar manualmente os campos
+    inputTelefone.value = '';
 }
 
-function atualizaTabela() { //este trecho estava inicialmente junto da função 'adicionaLinha', mas ela ficou exclusiva para adicionar LINHA, enquanto esta função irá adicionar CONTEÚDO às linhas criadas
-    const corpoTabela = document.querySelector('tbody'); //criando constante para inserir o conteúdo dentro do corpo da tabela
+function atualizaTabela() { 
+    const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas; //para inserir um conteúdo dentro de uma tag utilizamos o atributo 'innerHTML'
-}
-
-function atualizaMediaFinal(){
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2); //'.toFixed()' delimita o número máximo de casas decimais - ex.: para não ficar 8.66666 e sim 8.66
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-}
-
-function calculaMediaFinal(){
-    let somaDasNotas = 0;
-
-    for (let i=0; i < notas.length; i++){ //isso é um laço
-        somaDasNotas += notas[i]; //'+=' é uma concatenação. Poderia ser: 'somaDasNotas = somaDasNotas + 'outro valor'
-    }
-
-    return somaDasNotas / notas.length;
 }
